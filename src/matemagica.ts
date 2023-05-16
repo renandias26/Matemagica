78df9bbeed0f23ba50b477dcf86b65cf4caf4e2c
Renan Dias <renan.dias@obraprimaweb.com.br>
2023-05-16 20:42:17 -0300

    first commit

---
+++ src/matemagica.ts
@@ -0,0 +1,46 @@
+class Matemagica{
+    private a: number
+    private b: number
+    
+    public SetA(number: number){
+        this.a = number
+    }
+
+    public SetB(number: number){
+        this.b = number
+    }
+
+    private Sum(){
+        return this.a + this.b
+    }
+
+    public GetSum(){
+        return this.Sum()
+    }
+
+    private Sub(){
+        return this.a - this.b
+    }
+
+    public GetSub(){
+        return this.Sub()
+    }
+
+    private Div(){
+        return this.a/ this.b
+    }
+
+    public getDiv(){
+        return this.Div()
+    }
+
+    private Mul(){
+        return this.a * this.b
+    }
+
+    public GetMul(){
+        return this.Mul()
+    }
+}
+
+export default Matemagica
